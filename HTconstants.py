f3af832db15a7a7844636b3817db1c189aa638dc
ivan hromada <ihromada@hotmail.com>
2014-10-08 00:32:45 -0700

    constants update

--- HTconstants.py
+++ HTconstants.py
@@ -2,10 +2,15 @@
 
 
 #Colors
-WHITE = (255,255,255)
 BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
 BLUE = (0,0,255)
-RED = (0,255,0)
+
+#Screen dimensions
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
 
 #Frames per second
 GAMEFPS = 30
